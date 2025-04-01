3056e0fb77fd8e80059f97a20dbe7d9532546499
JoaoFCosta <joaofiamenguicosta@gmail.com>
2025-04-01 09:21:00 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default App;
