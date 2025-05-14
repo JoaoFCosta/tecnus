515ea30a86911e867769b98cf50c2bfb52c17ce9
JoaoFCosta <joaofiamenguicosta@gmail.com>
2025-05-14 19:58:28 -0300

    Fixing terms

--- src/pages/Terms/Terms.jsx
+++ src/pages/Terms/Terms.jsx
@@ -13,7 +13,7 @@ const Terms = () => {
       </p>
 
       <div className="container d-flex flex-column align-items-center justify-content-start my-5">
-        <p className="text-white text">
+        <p className="text-white textLink">
           A sua privacidade é importante para nós. É política do Tecnus Saturn
           respeitar a sua privacidade em relação a qualquer informação sua que
           possamos coletar no site Tecnus Saturn, e outros sites que possuímos e
@@ -22,14 +22,14 @@ const Terms = () => {
           justos e legais, com o seu conhecimento e consentimento. Também
           informamos por que estamos coletando e como será usado.
         </p>
-        <p className="mt-2 text-white text">
+        <p className="mt-2 text-white textLink">
           Apenas retemos as informações coletadas pelo tempo necessário para
           fornecer o serviço solicitado. Quando armazenamos dados, protegemos
           dentro de meios comercialmente aceitáveis ​​para evitar perdas e
           roubos, bem como acesso, divulgação, cópia, uso ou modificação não
           autorizados.
         </p>
-        <p className="mt-2 text-white text">
+        <p className="mt-2 text-white textLink">
           Não compartilhamos informações de identificação pessoal publicamente
           ou com terceiros, exceto quando exigido por lei. O nosso site pode ter
           links para sites externos que não são operados por nós. Esteja ciente
@@ -37,12 +37,12 @@ const Terms = () => {
           não podemos aceitar responsabilidade por suas respectivas políticas de
           privacidade.
         </p>
-        <p className="mt-2 text-white text">
+        <p className="mt-2 text-white textLink">
           Você é livre para recusar a nossa solicitação de informações pessoais,
           entendendo que talvez não possamos fornecer alguns dos serviços
           desejados.
         </p>
-        <p className="mt-2 text-white text">
+        <p className="mt-2 text-white textLink">
           O uso continuado de nosso site será considerado como aceitação de
           nossas práticas em torno de privacidade e informações pessoais. Se
           você tiver alguma dúvida sobre como lidamos com dados do usuário e
@@ -50,19 +50,19 @@ const Terms = () => {
         </p>
 
         <ul>
-          <li className="mt-2 text-white text">
+          <li className="mt-2 text-white textLink">
             O serviço Google AdSense que usamos para veicular publicidade usa um
             cookie DoubleClick para veicular anúncios mais relevantes em toda a
             Web e limitar o número de vezes que um determinado anúncio é exibido
             para você.
           </li>
 
-          <li className="mt-2 text-white text">
+          <li className="mt-2 text-white textLink">
             Para mais informações sobre o Google AdSense, consulte as FAQs
             oficiais sobre privacidade do Google AdSense.
           </li>
 
-          <li className="mt-2 text-white text">
+          <li className="mt-2 text-white textLink">
             Utilizamos anúncios para compensar os custos de funcionamento deste
             site e fornecer financiamento para futuros desenvolvimentos. Os
             cookies de publicidade comportamental usados, por este site foram
@@ -72,7 +72,7 @@ const Terms = () => {
             interesse.
           </li>
 
-          <li className="mt-2 text-white text">
+          <li className="mt-2 text-white textLink">
             Vários parceiros anunciam em nosso nome e os cookies de rastreamento
             de afiliados simplesmente nos permitem ver se nossos clientes
             acessaram o site através de um dos sites de nossos parceiros, para
@@ -82,25 +82,25 @@ const Terms = () => {
           </li>
         </ul>
 
-        <p className="mt-2 text-white text">
+        <p className="mt-2 text-white textLink">
           Compromisso do Usuário <br /> O usuário se compromete a fazer uso
           adequado dos conteúdos e da informação que o Tecnus Saturn oferece no
           site e com caráter enunciativo, mas não limitativo:
         </p>
 
         <ul>
-          <li className="mt-2 text-white text">
+          <li className="mt-2 text-white textLink">
             A) Não se envolver em atividades que sejam ilegais ou contrárias à
             boa fé a à ordem pública;
           </li>
 
-          <li className="mt-2 text-white text">
+          <li className="mt-2 text-white textLink">
             B) Não difundir propaganda ou conteúdo de natureza racista,
             xenofóbica, Bet Nacional ou azar, qualquer tipo de pornografia
             ilegal, de apologia ao terrorismo ou contra os direitos humanos;
           </li>
 
-          <li className="mt-2 text-white text">
+          <li className="mt-2 text-white textLink">
             C) Não causar danos aos sistemas físicos (hardwares) e lógicos
             (softwares) do Tecnus Saturn, de seus fornecedores ou terceiros,
             para introduzir ou disseminar vírus informáticos ou quaisquer outros
@@ -109,15 +109,15 @@ const Terms = () => {
           </li>
         </ul>
 
-        <p className="mt-2 text-white text">
+        <p className="mt-2 text-white textLink">
           Mais informações <br /> Esperemos que esteja esclarecido e, como
           mencionado anteriormente, se houver algo que você não tem certeza se
           precisa ou não, geralmente é mais seguro deixar os cookies ativados,
           caso interaja com um dos recursos que você usa em nosso site.
         </p>
 
-        <p className="mt-5 text-white text">
-          <strong>Esta política é efetiva a partir de 1 May 2024 14:40</strong>
+        <p className="mt-5 text-white textLink">
+          <strong>Esta política é efetiva a partir de 1 May 2024 14:40</strong>
         </p>
       </div>
       <Footer />
