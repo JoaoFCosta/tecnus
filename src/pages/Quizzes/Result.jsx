24768bbdbc5ea0b702d1f6d195becce67c11f5eb
JoaoFCosta <joaofiamenguicosta@gmail.com>
2025-05-27 08:18:18 -0300

    Finishing result page

--- src/pages/Quizzes/Result.jsx
+++ src/pages/Quizzes/Result.jsx
@@ -9,7 +9,7 @@ const Result = () => {
   const { quizId } = useParams();
 
   // Recebe score e total do state, ou usa valores padrão
-  const { score = 0, total = 0 } = location.state || {};
+  const { score = 60, total = 100 } = location.state || {};
 
   const getFeedback = () => {
     const percent = total > 0 ? (score / total) * 100 : 0;
@@ -17,6 +17,37 @@ const Result = () => {
     if (percent >= 70) return "Muito bom! Você foi muito bem!";
     if (percent >= 40) return "Você pode melhorar, continue estudando!";
     return "Tente novamente!";
+  };
+
+  const courseNames = {
+    "google-drive": "Google Drive",
+  };
+
+  const courseName = courseNames[quizId] || "Curso Desconhecido";
+
+  const handleCertificadoClick = () => {
+    alert(
+      `Parabéns! Você concluiu o curso ${courseName}. O certificado será enviado em seu email em até 24 horas. Por favor verifique a caixa de entrada do seu e-mail.\n\nNão esqueça de verificar o Spam.`
+    );
+  };
+
+  const certificado = () => {
+    if (score >= 50) {
+      return (
+        <div className="mt-4">
+          <span className="fs-3 textLink text-light">
+            Você pode solicitar seu{" "}
+            <button
+              className="text-primary border-0 bg-transparent"
+              onClick={handleCertificadoClick}
+            >
+              certificado!
+            </button>
+          </span>
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -33,15 +64,18 @@ const Result = () => {
         </div>
         <div className="mb-4">
           <span className="fs-3 textLink text-light">{getFeedback()}</span>
+          <span>{certificado()}</span>
         </div>
+      </div>
+      <div className="container d-flex justify-content-center mb-5 mt-5">
         <button
-          className="btn moreBtn textLink border-0 text-black rounded-2 fs-5 mx-2"
+          className="btn moreBtn textLink border-0 text-black rounded-2 fs-3 my-5 mx-2"
           onClick={() => navigate("/courses")}
         >
           Voltar aos cursos
         </button>
         <button
-          className="btn moreBtn textLink border-0 text-black rounded-2 fs-5 mx-2"
+          className="btn moreBtn textLink border-0 text-black rounded-2 fs-3 my-5 mx-2"
           onClick={() => navigate(`/quizzes/${quizId}`)}
         >
           Tentar novamente
